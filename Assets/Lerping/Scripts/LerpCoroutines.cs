1fa3be3457ae0a94b41d688731ba8e853302d7e5
deepak123 <deepak.kalyani@imuons.com>
2021-03-16 15:21:56 +0530

    lerp

---
+++ Assets/Lerping/Scripts/LerpCoroutines.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class LerpCoroutines : MonoBehaviour
+{
+
+    public Vector3 startScale, endScale;
+    public float duration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ScaleFlipAnimation(startScale, endScale, gameObject, duration));
+    }
+    IEnumerator ScaleFlipAnimation(Vector3 initialScale, Vector3 finalScale, GameObject obj, float timeDuration)
+    {
+        float progress = 0;
+        while (progress <= timeDuration)
+        {
+            obj.transform.localScale = Vector3.Lerp(initialScale, finalScale, progress); //scale lerping...
+            progress += Time.deltaTime;
+            //score.text = Mathf.FloorToInt(Mathf.Lerp(0,23,progress)).ToString(); //sore lerping...
+            yield return null;
+        }
+        progress = 0;
+        while (progress <= timeDuration)
+        {
+            obj.transform.localScale = Vector3.Lerp(finalScale, initialScale, progress); //scale lerping...
+            progress += Time.deltaTime;
+            //score.text = Mathf.FloorToInt(Mathf.Lerp(23, 0, progress)).ToString(); //sore lerping...
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Lerping/Scripts/ScoreLerp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ScoreLerp : MonoBehaviour
+{
+    public float duration;
+    public TMP_Text score;
+    float prog = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(Scorelerp(0, 120, duration));
+    }
+    IEnumerator Scorelerp(int startScore, int endScore, float duration)
+    {
+        float progress = 0;
+        while (progress <= duration)
+        {
+            progress += Time.deltaTime;
+            score.text = Mathf.FloorToInt(Mathf.Lerp(startScore, endScore, progress)).ToString();
+            yield return null;
+        }
+    }
+    private void Update()
+    {
+        if(prog<=duration)
+        {
+            prog += Time.deltaTime;
+            score.text = Mathf.FloorToInt(Mathf.Lerp(12, 120, prog)).ToString();
+        }
+    }
+}
