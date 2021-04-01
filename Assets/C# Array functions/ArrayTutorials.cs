b49f206129b44fb522d65793c29bb8b7b563ea78
deepak123 <deepak.kalyani@imuons.com>
2021-04-01 11:31:59 +0530

    update lerp score

---
+++ Assets/C# Array functions/ArrayTutorials.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ArrayTutorials : MonoBehaviour
+{
+    private Person[] persons = { new Person("Sagar",1,false), new Person("Ganesh", 0,true) , new Person("Mayur", 2,false) , new Person("Deepak", 3,true), new Person("Nikhil", 4, true) };
+    // Start is called before the first frame update
+    void Start()
+    {
+        Person person = System.Array.Find(persons,p=>p.id==3);
+        Debug.Log("Found person->"+person.name);
+        int index = System.Array.FindIndex(persons,p=>p.id==0);
+        Debug.Log("Person with id 0 is in the "+index+" position in the list");
+        Person[] openedUsers = System.Array.FindAll(persons,p=>p.isOpen==true);
+        int i = 0;
+        while(i<10)
+        {
+            if (i == 5) { i++; continue; }
+            Debug.Log("->" + i);
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public class Person
+{
+    public string name;
+    public int id;
+    public bool isOpen;
+    public Person(string name,int id,bool isopen)
+    {
+        this.name = name;
+        this.id = id;
+        this.isOpen = isopen;
+    }
+}
--- Assets/Lerping/Scripts/ScoreLerp.cs
+++ Assets/Lerping/Scripts/ScoreLerp.cs
@@ -2,32 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class ScoreLerp : MonoBehaviour
 {
     public float duration;
     public TMP_Text score;
+    public Button lerpBTN;
+    public int start, end;
     float prog = 0;
     // Start is called before the first frame update
     void Start()
     {
-        //StartCoroutine(Scorelerp(0, 120, duration));
+        
+    }
+    public void LerpScore()
+    {
+        lerpBTN.interactable = false;
+        StartCoroutine(Scorelerp(start, end, duration));
     }
     IEnumerator Scorelerp(int startScore, int endScore, float duration)
     {
         float progress = 0;
+        yield return new WaitForSeconds(1);
         while (progress <= duration)
         {
             progress += Time.deltaTime;
             score.text = Mathf.FloorToInt(Mathf.Lerp(startScore, endScore, progress)).ToString();
             yield return null;
         }
+        lerpBTN.interactable = true;
     }
     private void Update()
     {
-        if(prog<=duration)
+        /*if(prog<=duration)
         {
             prog += Time.deltaTime;
             score.text = Mathf.FloorToInt(Mathf.Lerp(12, 120, prog)).ToString();
-        }
+        }*/
     }
 }
