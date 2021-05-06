6c0a9a56c794ed7c2517a9fdea2225b440fa8147
deepak123 <deepak.kalyani@imuons.com>
2021-05-06 16:41:44 +0530

    scroll

---
+++ Assets/swipe controlled ScrollRect with items snapping/Scripts/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+namespace SSR
+{
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField] private string[] messages;
+        [SerializeField] private TMP_Text msg;
+        // Start is called before the first frame update
+        private void OnEnable()
+        {
+            ScrollSnapRect.GetCurrentPage += OnPageChanged;
+            msg.text = messages[0];
+        }
+
+        private void OnPageChanged(int page)
+        {
+            msg.text = messages[page];
+        }
+
+        private void OnDisable()
+        {
+            ScrollSnapRect.GetCurrentPage -= OnPageChanged;
+        }
+    }
+}
